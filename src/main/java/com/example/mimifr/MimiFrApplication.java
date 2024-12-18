577486c7f4917caaa0602a16c6473c0dfcbc01cc
Kim Daul <elre519@naver.com>
2024-12-18 11:48:12 +0900

    Fr

---
+++ src/main/java/com/example/mimifr/MimiFrApplication.java
@@ -0,0 +1,13 @@
+package com.example.mimifr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MimiFrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MimiFrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mimifr/MimiFrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mimifr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MimiFrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
